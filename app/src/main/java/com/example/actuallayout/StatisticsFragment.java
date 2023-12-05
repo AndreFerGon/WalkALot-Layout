bec3a605700c730ed8658252f4b2ca7ca61ad69a
franciscosacouto <francisco.sa.couto42@gmail.com>
2023-12-05 04:00:26 +0100

    BarChart adicionado

--- app/src/main/java/com/example/actuallayout/StatisticsFragment.java
+++ app/src/main/java/com/example/actuallayout/StatisticsFragment.java
@@ -1,12 +1,27 @@
 package com.example.actuallayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +34,8 @@ public class StatisticsFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private List<String> xValues = Arrays.asList("Sunday","Monday","Tuesday", "Wednesday","Thursday");
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -43,6 +60,7 @@ public class StatisticsFragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+
     }
 
     @Override
@@ -52,12 +70,57 @@ public class StatisticsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_statistics, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        BarChart barChart = (BarChart) getView().findViewById(R.id.chart);
+        barChart.getAxisRight().setDrawLabels(false);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0,45f));
+        entries.add(new BarEntry(1,80f));
+        entries.add(new BarEntry(2,65f));
+        entries.add(new BarEntry(3,38f));
+        entries.add(new BarEntry(4,70f));
+
+        YAxis yAxis = barChart.getAxisLeft();
+        yAxis.setAxisMaximum(0f);
+        yAxis.setAxisMaximum(100f);
+        yAxis.setAxisLineWidth(2f);
+        yAxis.setTextColor(Color.WHITE);
+        yAxis.setAxisLineColor(Color.BLACK);
+        yAxis.setLabelCount(10);
+
+        BarDataSet dataSet = new BarDataSet(entries,"Subjects");
+        /*dataSet.setColors(ColorTemplate.MATERIAL_COLORS);*/
+
+        BarData barData = new BarData(dataSet);
+        barChart.setData(barData);
+
+        barChart.getDescription().setEnabled(false);
+        barChart.invalidate();
+
+        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(xValues));
+        barChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        barChart.getXAxis().setGranularity(1f);
+        barChart.getLegend().setTextColor(android.R.color.white);
+        barChart.getXAxis().setTextColor(Color.WHITE);
+        barChart.getXAxis().setGranularityEnabled(true);
+        barChart.getDescription().setTextColor(Color.WHITE);
+        barData.setValueTextColor(Color.WHITE);
+        barData.setValueTextSize(10f);
     }
 }
